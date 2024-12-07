aecb82a5ddfbf088f0a68c9f1a2910c6f9f5d33f
MarkJoseph12345 <markjosephpiodos@gmail.com>
2024-12-07 10:18:19 +0800

    Initial Commit

--- Diabetes.py
+++ Diabetes.py
@@ -72,7 +72,7 @@ plt.title("Correlation Heatmap of Features")
 st.pyplot(plt)
 
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1412)
 
 model = LinearRegression()
 model.fit(x_train, y_train)
